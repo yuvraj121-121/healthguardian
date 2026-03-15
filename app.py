729f4d1d1dda61c591c0e57d357ccc5fbbcd4b9a
yuvraj121-121 <72939314+yuvraj121-121@users.noreply.github.com>
2026-03-15 19:27:02 +0530

    fix: autofill style in forgot and reset password

--- app.py
+++ app.py
@@ -16,11 +16,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_EMAIL')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_EMAIL')
+app.config['MAIL_TIMEOUT'] = 10
 
 mail.init_app(app)
 login_manager.init_app(app)
